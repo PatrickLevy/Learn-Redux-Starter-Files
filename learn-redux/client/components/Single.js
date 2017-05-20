499a13f32f5944566bafe045f4ea5dfbe0cd68a7
Patrick Levy <mrpatricklevy@gmail.com>
2017-05-20 10:25:44 -0500

    End of video 16 - functional app

---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Photo from './photo.js';
+import Comments from './Comments.js';
+// import comments
+
+const Single = React.createClass({
+    render() {
+        // index of the post
+        const { postId } = this.props.params;
+        const i = this.props.posts.findIndex((post) => post.code ===
+            postId);
+
+        console.log(i);
+
+        const post = this.props.posts[i];
+
+        const postComments = this.props.comments[postId] || [];
+        console.log('postComments', postComments);
+
+
+        console.log(post);
+        // get us the post
+        return (
+            <div className="single-photo">
+                <Photo i={i} post={post} {...this.props} />
+                <Comments postComments={postComments} {...this.props}/>
+            </div>
+        )
+    }
+});
+
+export default Single;
---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,38 @@
+
+// Look up reducer composition!
+function postComments(state = [], action) {
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            return [...state, {
+                user: action.author,
+                text: action.comment,
+            }];
+        case 'REMOVE_COMMENT':
+            console.log('removing a comment');
+            return [
+                // from the start to the one we want to delete
+                ...state.slice(0, action.i),
+                // after the one we deleted
+                ...state.slice(action.i + 1),
+            ];
+        default:
+            return state;
+
+    }
+    return state;
+}
+
+function comments(state = [], action) {
+    if (typeof action.postId !== 'undefined') {
+        return {
+            // take the current state
+            ...state,
+            //overwrite this post with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
+
+    return state;
+}
+
+export default comments;
